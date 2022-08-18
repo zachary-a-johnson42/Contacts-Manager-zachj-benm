09186691a4fa2b130503c80732881876bf6b59cb
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-08-18 14:23:35 -0500

    added getall and getbyname to fileio

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,22 @@
 package Main;
 
+import file_io.ContactList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: SET UP MAIN MENU FOR USER SELECTIONS
+        ContactList contacts = new ContactList();
+        //Working as intender
+        contacts.getAllContacts(contacts.contactsFilePath);
 
+        //EXPECT 1 line
+        contacts.getContactByName("Ben");
         /*  1. View contacts.
             2. Add a new contact.
             3. Search a contact by name.
