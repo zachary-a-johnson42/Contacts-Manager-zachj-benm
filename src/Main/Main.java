98c193a28613a9ef0bc4a59566f9c5fa77ce487d
Benjamin Marks <bjmarks13@yahoo.com>
2022-08-18 16:28:30 -0500

    Main class

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,19 +1,30 @@
 package Main;
 
+import contacts.Contact;
+import file_io.ContactList;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: SET UP MAIN MENU FOR USER SELECTIONS
 
+        ContactList contactList = new ContactList();
+
+        contactList.getAllContacts();
+
+        contactList.deleteContactByName("Zach");
+
+        contactList.getAllContacts();
         /*  1. View contacts.
             2. Add a new contact.
             3. Search a contact by name.
             4. Delete an existing contact.
             5. Exit.
             Enter an option (1, 2, 3, 4 or 5): */
+        contactList.getAllContacts();
 
         // TODO : SWITCH STATEMENT FOR USER INPUT
 
-            // LOGIC FOR EACH CHOICE
+        // LOGIC FOR EACH CHOICE
 
         // TODO : PROMPT TO CONTINUE OR QUIT
 
